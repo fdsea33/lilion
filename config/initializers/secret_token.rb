9b8d0734c59c9a818fea0cc0051d4f3fe4c89f35
Brice Texier <brice.texier@ekylibre.org>
2011-06-06 14:18:55 +0200

    Generate empty Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lilion::Application.config.secret_token = '370e174047fa48ee294bf09321bc8748b0db82da12199f85781d6a5344d2726d2706896601f6e912aee2b6c81b137c2abab776b5b7eedffe4738c9e4319307a9'
